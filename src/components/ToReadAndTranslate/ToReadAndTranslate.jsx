8d4572aeafee7476cffc1a237ced7fe32669c7fe
YUGCHR <47811171+YUGCHR@users.noreply.github.com>
2020-03-31 00:38:10 +0100

    Tested Post and inserted id++

--- src/components/ToReadAndTranslate/ToReadAndTranslate.jsx
+++ src/components/ToReadAndTranslate/ToReadAndTranslate.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Axios from 'axios';
 import s from './ToReadAndTranslate.module.css'
 
 const Sentence = (props) => {
@@ -16,7 +17,7 @@ let ToReadAndTranslate = (props) => {
     let sentencesOnPageTop = props.readAndTranslatePage.sentencesOnPageTop;
     let startSentenceTop = readingSentence - sentencesOnPageTop;
     let endSentenceTop = startSentenceTop + sentencesOnPageTop;
-    debugger;
+
     let showTopSentences = props.readAndTranslatePage.engSentences.slice(startSentenceTop, endSentenceTop).map((sts) => {
         return <Sentence text={sts.sentenceText} />
     });
@@ -29,55 +30,62 @@ let ToReadAndTranslate = (props) => {
         return <Sentence text={sbs.sentenceText} />
     });
 
-    let currentEngSentence = (props) => { return <Sentence text={props.sentenceText} /> };
+    let currentEngSentence = (props) => { return <Sentence text={props.id + props.sentenceText} /> };
     let showCurrentEngSentenceContext = currentEngSentence(props.readAndTranslatePage.engSentences[readingSentence]);
 
     let currentRusSentence = [props.readAndTranslatePage.rusSentences[readingSentence]].map((crs) => {
-        return <Sentence text={crs.sentenceText} />
+        return <Sentence text={crs.id + crs.sentenceText} />
     });
 
-    return (<div>
-        <div className={s.twoColumnsReading}>
-            {lastSentenceNumber}
-        </div>
-        <div className={s.editLine}>
-            Edit line
+    props.readAndTranslatePage.engSentences.map(u => {
+        Axios
+            .post("https://localhost:5001/api/TodoItems", u)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error))}) 
+        
+        return (<div>
+            <div className={s.twoColumnsReading}>
+                {lastSentenceNumber}
             </div>
-        <div>
+            <div className={s.editLine}>
+                Edit line
+            </div>
             <div>
-                <div className={s.twoColumnsTop}>
+                <div>
+                    <div className={s.twoColumnsTop}>
+                        <p>
+                            <button onClick={() => { props.scrollLineUp(readingSentence) }}>Scroll Line Up</button>
+                        </p>
+                    </div>
+                    <div>
+                        {showTopSentences}
+                    </div>
+                </div>
+            </div>
+            <div className={s.twoColumnsReading}>
+                {readingSentence}
+            </div>
+            <div className={s.currentSentence}>
+                {showCurrentEngSentenceContext}
+            </div>
+
+            <div>
+            </div>
+            <div className={s.translateLine}>
+                {currentRusSentence}
+            </div>
+            <div>
+                <div>
+                    <div>{showBottomSentences}</div>
+                </div>
+                <div className={s.twoColumnsBottom}>
                     <p>
-                        <button onClick={() => { props.scrollLineUp(readingSentence) }}>Scroll Line Up</button>
+                        <button onClick={() => { props.scrollLineDown(readingSentence) }}>Scroll Line Down</button>
                     </p>
                 </div>
-                <div>
-                    {showTopSentences}
-                </div>
             </div>
-        </div>
-        <div className={s.twoColumnsReading}>
-            {readingSentence}
-        </div>
-        <div className={s.currentSentence}>
-            {showCurrentEngSentenceContext}
-        </div>
-
-        <div>
-        </div>
-        <div className={s.translateLine}>
-            {currentRusSentence}
-        </div>
-        <div>
-            <div>
-                <div>{showBottomSentences}</div>
-            </div>
-            <div className={s.twoColumnsBottom}>
-                <p>
-                    <button onClick={() => { props.scrollLineDown(readingSentence) }}>Scroll Line Down</button>
-                </p>
-            </div>
-        </div>
-    </div >)
-}
+        </div >)
+    }
 
 export default ToReadAndTranslate;
